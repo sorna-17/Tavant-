8bd5addd5ba223b4be71c6bbf0618544713f2313
Sornam Thiyagarajan <36667578+sorna-17@users.noreply.github.com>
2019-08-10 13:18:49 +0530

    Update Temperature.java

--- src/day2/Temperature.java
+++ src/day2/Temperature.java
@@ -2,12 +2,12 @@ package day2;
 
 public class Temperature {
 	
-	public  double ConvertToFarenheit(double celsius){
+	public  double CelsiusToFarenheit(double celsius){
 		  
 		 return ( ((celsius*9)/5)+32 );
 	  }
 	
-	 public  double ConvertToCelsius(double farenheit) {
+	 public  double FarenheitToCelsius(double farenheit) {
 		  
 		  return ( (farenheit-32)*5/9);
 	  }
